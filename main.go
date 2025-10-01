f092deb85e8ab1f51a2de4b1002e7efa7284c262
ArtemMaliutin <mayartmal@gmail.com>
2025-10-01 17:32:55 +0400

    add db and basic logic

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"example.com/rest-api/db"
+	"example.com/rest-api/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
+
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 	server.Run(":8080") // localhost:8080
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error during DB reading"})
+		fmt.Println(err)
+		return
+	}
+	context.JSON(http.StatusOK, events)
 
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error during DB writing"})
+		fmt.Println(err)
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "Event Created", "event": event})
 }
